21f86003095055a051574b6c5d95eec373e3f00d
batunet <batunet@batunet-MacBook-Pro.local>
2018-08-30 18:31:30 +0300

    added new icons

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+export default  {
+    images:{
+        heartIcon:require('../../assets/heart.png'),
+        chatIcon:require('../../assets/message.png'),
+        bubbleIcon:require('../../assets/arrow.png')
+    }
+}
