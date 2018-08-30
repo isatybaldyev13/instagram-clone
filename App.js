d3b393fea3ccc268565a4237f9b1f54f4fe6a185
batunet <batunet@batunet-MacBook-Pro.local>
2018-08-30 18:11:06 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import InstaClone from './src/instaClone'
+
+export default class App extends Component {
+  render() {
+    return (
+      <InstaClone />
+    );
+  }
+}
+
